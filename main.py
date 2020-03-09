50bd7a1515a5be1897ff0af54cec95b4a8eee561
Aaron <55195129+aalavi16@users.noreply.github.com>
2020-03-09 10:11:30 +0000

    fixed adding / minusing 5 points from cpu
    also cleaned things up

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ global point
 global round
 global roll
 
-
-
+playerWins = 0
+cpuWins = 0
+draws = 0
+p1FinalTotal = 0
+cpuFinalTotal = 0
 
 score = 0
 highscore = 0
@@ -28,8 +31,8 @@ def login():
   database = csv.reader(check,delimiter=',')
   loggedin1 = False
   while loggedin1==False:
-    username = input("Please enter your username ")
-    password = input("Please enter your password ")
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
     for row in database:
       usernameF = row[0]
       passwordF = row[1]
@@ -41,7 +44,7 @@ def login():
           login2P()
     if loggedin1 == False and loginA<4:
       print('Failed to sign in. Please try again') 
-      loginA = loginA + 1
+      #loginA = loginA + 1
       login()
     elif loggedin1 == True:
       sleep(1)
@@ -65,8 +68,8 @@ def login2P():
   database = csv.reader(check,delimiter=',')
   loggedin2 = False
   while loggedin2==False:
-    username = input("Please enter your username, player 2")
-    password = input("Please enter your password, player 2")
+    username = input("Please enter your username, player 2: ")
+    password = input("Please enter your password, player 2: ")
     for row in database:
       usernameF = row[0]
       passwordF = row[1]
@@ -75,7 +78,7 @@ def login2P():
         print("Welcome " + username + "!")
         sleep(1)
     if loggedin2 == False and loginA<4:
-      print('Failed to sign in. Please try again') 
+      print('Failed to sign in. Please try again...') 
       loginA = loginA + 1
       login()
     #elif loggedin2 == False and loginA == 3:
@@ -102,8 +105,15 @@ def menu1pb():
     exit()
   
 def menu1pa():
-
-  choice = int(input("Do you want to:\n1) Login\n2) Register "))
+  while True:
+    try:
+      choice = int(input("Do you want to:\n1) Login\n2) Register\nChoice: "))
+    except ValueError:
+      print("Try again!")
+      continue
+    else:
+      break
+  
   if choice == 1:
     login()
   elif choice == 2:
@@ -115,13 +125,23 @@ def menu1pa():
 
 def mainmenu():
   global multiplayer
-  choice = int(input("1) 1 Player\n2) 2 Players\nChoice:  "))
+  while True:
+    try:
+      choice = int(input("1) 1 Player\n2) 2 Players\nChoice:  "))
+    except ValueError:
+      print("Try again!")
+      continue
+    else:
+      break
+
+
   if choice == 1:
     menu1pa()
   elif choice == 2:
     #menu2p()
     print("\033[1;31;1mWIP")
     multiplayer = True
+
 
 
 def register():
@@ -146,13 +166,44 @@ def register():
   login()
 
 def finalResults():
-  print("FINAL RESULTS!")
-  if draws == 5:
-    print("NO-ONE WINS!")
-  if cpuTotal > player1Total:
-    print("BETTER LUCK NEXT TIME. THE WORLD CHAMPION IS THE CPU!")
-  elif player1Total:
-    print("YAY! YOU WIN!")
+  global draws
+  global cpuWins
+  global playerWins
+  global p1FinalTotal
+  global cpuFinalTotal
+  global round
+  print("\n")
+  print("\033[0;31;1mFINAL RESULTS!")
+  if draws >= 3:
+    print("\033[0;31;1mNO-ONE WINS!")
+  elif cpuWins >= 3:
+    print("\033[0;33;1mBETTER LUCK NEXT TIME. THE CHAMPION IS THE CPU!")
+  elif playerWins >= 3:
+    print("\033[0;34;1mYAY! YOU WIN!")
+  print("\n")
+  print("\033[0;34;1mYour final total is" , p1FinalTotal)
+  print("\n")
+  print("\033[0;33;1mThe CPU's final total is" , cpuFinalTotal)
+  print("\n")
+  print("\033[0;34;1mYou won " + str(playerWins) + " time(s).")
+  print("\n")
+  print("\033[0;33;1mThe CPU won " + str(cpuWins) + " time(s).")
+  print("\n")
+  print("\033[0;31;1mYou drew " + str(draws) + " time(s).")
+  print("\n")
+  choice12 = input("Do you want to play again? ")
+  if choice12 == "Yes":
+    round = 1
+    playerWins = 0
+    cpuWins = 0
+    draws = 0
+    p1FinalTotal = 0
+    cpuFinalTotal = 0
+    print("\033[2J")
+    game()
+  else:
+    exit()
+
 
 
 def resultsRoll():
@@ -187,30 +238,38 @@ def game():
   global draws
   """Player One Game"""
   global roll
+  global p1FinalTotal
+  global cpuFinalTotal
+  
   point = 0
   player1Total = 0
   cpuTotal = 0
  # pointlist = []
   roll = 1
-  print("ROUND" , round)
-  print("PLAYER TURN")
+  print("\n")
+  print("\033[0;31;1mROUND" , round)
+  print("\n")
+  print("\033[0;34;1mPLAYER TURN")
   #rollChoice = ("Do you want to:\nplay it safe (spin)\nor take a risk (roll)? ")
   input("Press enter to roll the die ")
-  sleep(1)
+  sleep(0.5)
   point1 = random.randint(1,6)
   player1Total += point1
   point = point1
   resultsRoll()
   cpuroll = 1
-  print("CPU TURN")
-  sleep(1)
+  print("\n")
+  sleep(0.5)
+  print("\033[0;33;1mCPU TURN")
+  sleep(0.5)
   cpupoint1 = random.randint(1,6)
   cpuTotal += cpupoint1
   print("The CPU got" , cpupoint1)
   print("Their total is" , cpuTotal)
-  print("PLAYER TURN 2")
+  print("\n")
+  print("\033[0;34;1mPLAYER TURN 2")
   input("Press enter to roll the die (Roll 2)")
-  sleep(1)
+  sleep(0.5)
   roll = 2
   
   point2 = random.randint(1,6)
@@ -218,29 +277,42 @@ def game():
   point = point2
   resultsRoll()
   if point1 == point2:
+    print("\n")
     print("You got two of the same in a row!")
-    print("PLAYER TURN 3!")
+    print("\033[0;34;1mPLAYER TURN 3!")
+    input("Press enter to roll the die (Bonus Roll)")
     roll = 3
     point = random.randint(1,6)
     player1Total += point
     resultsRoll()
-  print("CPU TURN 2")
+  print("\n")
+  sleep(0.5)
+  print("\033[0;33;1mCPU TURN 2")
   cpuroll = 2
-  sleep(1)
+  sleep(0.5)
   cpupoint2 = random.randint(1,6)
   cpuTotal += cpupoint2
   print("The CPU got" , cpupoint2)
   print("Their total is" , cpuTotal)
   if cpupoint1 == cpupoint2:
+    print("\n")
     print("The CPU got two of the same numbers in a row!")
-    print("CPU TURN 3!")
+    sleep(0.5)
+    print("\033[0;33;1mCPU TURN 3!")
     cpuroll = 3
     point = random.randint(1,6)
     cpuTotal += point
+    sleep(0.5)
     print("The CPU got" , point)
     print("Their total is" , cpuTotal)
-  print("ROUND RESULTS!")
-  print("PLAYER RESULTS:")
+  print("\n")
+  sleep(0.5)
+  print("\033[0;31;1mROUND RESULTS!")
+  sleep(0.5)
+  print("\n")
+  print("\033[0;34;1mPLAYER RESULTS:")
+  sleep(0.5)
+  p1FinalTotal += player1Total
   if player1Total % 2 == 0:
     print("Your total is even!")
     player1Total += 10
@@ -251,33 +323,50 @@ def game():
     if player1Total < 0:
       player1Total = 0
     print("5 points have been subtracted! Your round total is:" , player1Total)
-  print("CPU RESULTS:")
+  print("\n")
+  sleep(0.5)
+  print("\033[0;33;1mCPU RESULTS:")
+  sleep(0.5)
+  cpuFinalTotal += cpuTotal
   if cpuTotal % 2 == 0:
     print("The CPU's total is even!")
     cpuTotal += 10
     print("10 points have been added! Their round total is:" , cpuTotal)
   else:
     print("The CPU's total is odd!")
-    player1Total -= 5
-    if player1Total < 0:
-      player1Total = 0
+    cpuTotal -= 5
+    if cpuTotal < 0:
+      cpuTotal = 0
     print("5 points have been subtracted! Their round total is:" , cpuTotal)
+  sleep(0.5)
   if cpuTotal == player1Total:
-    print("THIS ROUND WAS A DRAW!")
-    draws = 1
+    print("\n")
+    print("\033[0;31;1mTHIS ROUND WAS A DRAW!")
+    draws += 1
     round += 1
-    game()
+    if round > 5:
+      finalResults()
+    else:
+      game()
     # if round = 5 do final results
   elif player1Total > cpuTotal:
-    print("YOU WIN THIS ROUND!")
-    playerWins = 1
+    print("\n")
+    print("\033[0;34;1mYOU WIN THIS ROUND!")
+    playerWins += 1
     round += 1
-    game()
-  elif cpuTotal > player1Total:
-    print("YOU LOST THIS ROUND!")
-    cpuWins = 1
+    if round > 5:
+      finalResults()
+    else:
+      game()
+  elif player1Total < cpuTotal:
+    print("\n")
+    print("\033[0;33;1mYOU LOST THIS ROUND!")
+    cpuWins += 1
     round += 1
-    game()
+    if round > 5:
+      finalResults()
+    else:
+      game()
   
 
           
